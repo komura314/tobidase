2c9943661bde648fcd86aeba339306b1b490e47d
komura314 <kom314.prog@gmail.com>
2021-12-18 02:27:39 +0000

    feat: ビデオを画面読み込み時に１度だけ再生

--- src/components/AppTop.vue
+++ src/components/AppTop.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="scroll-top">
-    <video width="100%" src="/videos/tobidase.mp4" autoplay playsinline muted />
+    <video muted playsinline prelod="auto" class="el-video" width="100%">
+      <source src="/videos/tobidase.mp4" type="video/mp4">
+    </video>
   </div>
 </template>
 <script>
 export default {
   name: 'AppTop',
   data() {
-    return {}
+    return {
+      topVideo: false
+    }
+  },
+  mounted() {
+      this.topVideo = document.querySelector('.el-video');
+      this.topVideo.play()
   },
 }
 </script>
