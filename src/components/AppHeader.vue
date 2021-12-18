9adf9d1b79189c6136cbeb83eb05110f67b46de7
komura314 <kom314.prog@gmail.com>
2021-12-18 05:22:04 +0000

    feat: タイトルをボタンに変更

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,20 +1,27 @@
 <template>
   <v-app-bar app flat color="appHeader" extension-height="5">
-    <v-app-bar-nav-icon class="hidden-md-and-up" />
-    <v-container class="mx-auto py-0">
+    <v-container>
       <v-row align="center">
         <v-col>
-          <v-icon
-            large
-            class="mr-5"
-            height="48"
-            width="48"
-            max-width="48"
-            @click="onClick($event, links[0])"
+          <v-btn
+            rounded
+            text
+            x-large
+            class="grey--text text--darken-1"
+            @click="onClick(links[0])"
           >
-            mdi-door-open
-          </v-icon>
-          <v-btn text @click="onClick($event, links[0])"> TOBIDASE! </v-btn>
+            <v-icon
+              right
+              large
+              class="mr-5"
+              height="48"
+              width="48"
+              max-width="48"
+            >
+              mdi-door-open
+            </v-icon>
+            TOBIDASE!
+          </v-btn>
         </v-col>
 
         <v-spacer />
@@ -22,14 +29,13 @@
           <v-tabs
             v-model="tabSelected"
             background-color="appHeader"
-            class="hidden-sm-and-down "
+            class="hidden-sm-and-down"
           >
             <v-tab
               v-for="(link, i) in links"
               :key="i"
-              min-width="100"
               class="gray--text"
-              @click="onClick($event, link)"
+              @click="onClick(link)"
             >
               {{ link.text }}
             </v-tab>
@@ -84,9 +90,7 @@ export default {
     this.displayLoaded = true
   },
   methods: {
-    onClick(e, item) {
-      e.stopPropagation()
-
+    onClick(item) {
       if (item.to || !item.href) return
 
       this.$vuetify.goTo(item.href)
