b08aca85b3adcb572e03419087555b397dbde968
komura314 <kom314.prog@gmail.com>
2021-12-20 17:08:37 +0000

    feat: aboutの写真表示実装

--- src/components/AppAbout.vue
+++ src/components/AppAbout.vue
@@ -1,35 +1,24 @@
 <template>
   <div :id="compornentId" :ref="refId">
-    Aboutです
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+    <div class="text-caption mt-2">私について</div>
+    <div class="text-h4 md-2">ABOUT ME</div>
+    <v-divider />
+    <v-row align="center" justify="center">
+      <v-col cols="12" md="6">
+        <MyPhoto />
+      </v-col>
+    </v-row>
   </div>
 </template>
 <script>
 import { mapActions } from 'vuex'
 import { prefixCompornentId, prefixRefId, keyAbout } from '../plugins/feald'
+import MyPhoto from '../components/about/MyPhoto.vue'
 export default {
   name: 'AppAbout',
+  components: {
+    MyPhoto,
+  },
   data() {
     return {
       keyId: keyAbout,
---
+++ src/components/about/MyPhoto.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-carousel
+    v-model="selectMyPhoto"
+    interval="10000"
+    class="carousel-size my-10"
+    delimiter-icon="mdi-circle-medium"
+    hide-delimiter-background
+  >
+    <v-layout justify-center>
+      <v-carousel-item v-for="(photoPath, i) in myPhotos" :key="i">
+        <v-card shaped>
+          <v-img :src="photoPath" class="photo-size" />
+        </v-card>
+      </v-carousel-item>
+    </v-layout>
+  </v-carousel>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      selectMyPhoto: 0,
+      myPhotos: [
+        require('@/assets/images/me1.png'),
+        require('@/assets/images/me2.png'),
+        require('@/assets/images/me3.png'),
+      ],
+    }
+  },
+  computed: {
+    imageSrc: {
+      get() {
+        return this.myPhotos[this.selectMyPhoto]
+      },
+    },
+  },
+  methods: {
+    clickPhotoLeft() {
+      this.selectMyPhoto = this.selectMyPhoto - 1
+      if (this.selectMyPhoto < 0) this.selectMyPhoto = this.myPhotos.length - 1
+    },
+    clickPhotoRight() {
+      this.selectMyPhoto = this.selectMyPhoto + 1
+      if (this.selectMyPhoto >= this.myPhotos.length) this.selectMyPhoto = 0
+    },
+  },
+}
+</script>
+<style scoped>
+.carousel-size {
+  max-width: 500px;
+  max-height: 300px;
+}
+.photo-size {
+  max-height: 300px;
+  max-width: 300px;
+}
+</style>
