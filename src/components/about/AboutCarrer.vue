f46e804f7fb1888b75b7bfd449d0b202684ac587
komura314 <kom314.prog@gmail.com>
2021-12-22 17:15:28 +0000

    feat: about 経歴欄 作りこみ

--- src/components/about/AboutCarrer.vue
+++ src/components/about/AboutCarrer.vue
@@ -1,5 +1,38 @@
 <template>
   <div>
-    経歴について
+    <v-data-table
+      disable-filtering
+      disable-pagination
+      disable-sort
+      hide-default-footer
+      :headers="headers"
+      :items="items"
+    >
+    </v-data-table>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: '年月',
+          value: 'year',
+        },
+        {
+          text: '学歴・職歴',
+          value: 'carrer',
+        },
+      ],
+      items: [
+        { year: '2011年3月', carrer: '玉川大学 工学部 卒業' },
+        { year: '2011年4月', carrer: 'ソフトウェア情報開発（株） 入社' },
+        { year: '2014年3月', carrer: 'ソフトウェア情報開発（株） 退社' },
+        { year: '2015年9月', carrer: '公務員試験 小中学校事務 不合格' },
+        { year: '2015年10月', carrer: '株式会社コサウェル 入社' },
+      ],
+    }
+  },
+}
+</script>
