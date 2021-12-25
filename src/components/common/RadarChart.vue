7da90d61f4a677269ecac985f6120edad4466bd2
komura314 <kom314.prog@gmail.com>
2021-12-25 15:55:04 +0000

    feat: vue-chart.jsの追加

---
+++ src/components/common/RadarChart.vue
@@ -0,0 +1,19 @@
+<script>
+import { Radar } from 'vue-chartjs'
+export default {
+  extends: Radar,
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+    options: {
+      type: Object,
+      required: true,
+    },
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  },
+}
+</script>
