fb666c6dd47a38de917efe5b49393b532c92b62f
komura314 <kom314.prog@gmail.com>
2021-12-26 04:53:22 +0000

    feat: timelineのレーダチャートの内容更新

--- src/components/timeline/StepperContent.vue
+++ src/components/timeline/StepperContent.vue
@@ -6,10 +6,10 @@
       </v-col>
       <v-col cols="12" md="6">
         <v-row align="center" justify="center">
-          <v-col cols="12" md="6">
-            <StepperContentRadar :step="step" />
+          <v-col cols="12" md="12">
+            <StepperContentRadar :step="step"/>
           </v-col>
-          <v-col cols="12" md="6">
+          <v-col cols="12" md="12">
             <StepperContentRadarDetail :step="step" />
           </v-col>
         </v-row>
--- src/components/timeline/StepperContentRadarDetail.vue
+++ src/components/timeline/StepperContentRadarDetail.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-    {{ headers[0] }}{{ getSkillValue }} <br />
-    {{ headers[1] }}{{ getKnowledgeValue }} <br />
-    {{ headers[2] }}{{ getRecognitionValue }} <br />
-    {{ headers[3] }}{{ getStudyValue }} <br />
-    {{ headers[4] }}{{ getExperienceValue }} <br />
-  </div>
+  <v-card class="ma-5">
+    <v-card-text class="black--text">
+      {{ headers[0] }} {{ getSkillValue }} <br />
+      {{ headers[1] }} {{ getKnowledgeValue }} <br />
+      {{ headers[2] }} {{ getRecognitionValue }} <br />
+      {{ headers[3] }} {{ getStudyValue }} <br />
+      {{ headers[4] }} {{ getExperienceValue }} <br />
+    </v-card-text>
+  </v-card>
 </template>
 <script>
 import {
@@ -31,19 +33,34 @@ export default {
   },
   computed: {
     getSkillValue() {
-      return RADAR_SKILL_VALUES[RADAR_VALUES[this.step][0]]
+      return `
+        ${RADAR_VALUES[this.step][0]}：
+        ${RADAR_SKILL_VALUES[RADAR_VALUES[this.step][0]]}
+        `
     },
     getKnowledgeValue() {
-      return RADAR_KNOWLEDGE_VALUES[RADAR_VALUES[this.step][1]]
+      return `
+        ${RADAR_VALUES[this.step][1]}：
+        ${RADAR_KNOWLEDGE_VALUES[RADAR_VALUES[this.step][1]]}
+        `
     },
     getRecognitionValue() {
-      return RADAR_RECOGNITION_VALUES[RADAR_VALUES[this.step][2]]
+      return `
+        ${RADAR_VALUES[this.step][2]}：
+        ${RADAR_RECOGNITION_VALUES[RADAR_VALUES[this.step][2]]}
+        `
     },
     getStudyValue() {
-      return RADAR_STUDY_VALUES[RADAR_VALUES[this.step][3]]
+      return `
+        ${RADAR_VALUES[this.step][3]}：
+        ${RADAR_STUDY_VALUES[RADAR_VALUES[this.step][3]]}
+        `
     },
     getExperienceValue() {
-      return RADAR_EXPERIENCE_VALUES[RADAR_VALUES[this.step][4]]
+      return `
+        ${RADAR_VALUES[this.step][4]}：
+        ${RADAR_EXPERIENCE_VALUES[RADAR_VALUES[this.step][4]]}
+        `
     },
   },
 }
