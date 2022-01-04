f9206259399d097eeb604233cb13a003481a2fa5
komura314 <kom314.prog@gmail.com>
2022-01-04 17:38:50 +0000

    feat: workにgithubの項目追加

--- src/components/work/WorkPanel.vue
+++ src/components/work/WorkPanel.vue
@@ -5,14 +5,14 @@
     </v-expansion-panel-header>
     <v-expansion-panel-content>
       <v-card-text>
-        {{ workSubtitles[0] }}
-        <ul>
+        <span v-if="work.abouts">{{ workSubtitles[0] }}</span>
+        <ul v-if="work.abouts">
           <li v-for="(about, aboutId) in work.abouts" :key="'about' + aboutId">
             {{ about }}
           </li>
         </ul>
-        {{ workSubtitles[1] }}
-        <ul>
+        <span v-if="work.periods">{{ workSubtitles[1] }}</span>
+        <ul v-if="work.periods">
           <li
             v-for="(period, periodId) in work.periods"
             :key="'period' + periodId"
@@ -20,10 +20,16 @@
             {{ period }}
           </li>
         </ul>
-        {{ workSubtitles[2] }}
-        <ul>
+        <span v-if="work.skills">{{ workSubtitles[2] }}</span>
+        <ul v-if="work.skills">
           <li v-for="(skill, skillId) in work.skills" :key="'skill' + skillId">
             {{ skill }}
+          </li>
+        </ul>
+        <span v-if="work.github">{{ workSubtitles[3] }}</span>
+        <ul v-if="work.github">
+          <li>
+            <a :href="work.github.path" target="_blank">{{ work.github.text }}</a>
           </li>
         </ul>
       </v-card-text>
